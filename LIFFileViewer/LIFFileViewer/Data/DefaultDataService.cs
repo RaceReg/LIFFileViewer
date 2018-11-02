8445bbe5715068796873fc270f991d4a38db484d
Jackson Porter <jackson_porter@outlook.com>
2018-11-01 19:38:29 -0600

    Added interface and notimplemented data service

---
+++ LIFFileViewer/LIFFileViewer/Data/DefaultDataService.cs
@@ -0,0 +1,27 @@
+﻿using LIFFileViewer.Interfaces;
+using LIFFileViewer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFFileViewer.Data
+{
+    class DefaultDataService : IDataService
+    {
+        public bool FileExists(string lifFILE)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> FindFileAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Entry>> GetEntriesFromLIFAsync(string lifFILE)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LIFFileViewer/LIFFileViewer/Interfaces/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using LIFFileViewer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFFileViewer.Interfaces
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<Entry>> GetEntriesFromLIFAsync(string lifFILE);
+        bool FileExists(string lifFILE);
+        Task<string> FindFileAsync();
+    }
+}

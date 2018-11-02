98d9755c0ed70c292e40890b91c629fb2360e9dc
Jackson Porter <jackson_porter@outlook.com>
2018-11-01 18:41:24 -0600

    Added Entry class and basic UI

--- LIFFileViewer/LIFFileViewer/ViewModels/MainWindowViewModel.cs
+++ LIFFileViewer/LIFFileViewer/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LIFFileViewer.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace LIFFileViewer.ViewModels
@@ -7,5 +9,34 @@ namespace LIFFileViewer.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Hello World!";
+
+        private ObservableCollection<Entry> entries;
+        public ObservableCollection<Entry> Entries
+        {
+            get
+            {
+                return entries;
+            }
+            set
+            {
+                entries = value;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            entries = new ObservableCollection<Entry>();
+
+            var tempEntry = new Entry();
+            tempEntry.Place = 1;
+            tempEntry.RacerId = 0001;
+            tempEntry.Lane = 1;
+            tempEntry.LastName = "Porter";
+            tempEntry.FirstName = "Jackson";
+            tempEntry.Affiliation = "SC";
+            tempEntry.Time = DateTime.Now;
+
+            entries.Add(tempEntry);
+        }
     }
 }
